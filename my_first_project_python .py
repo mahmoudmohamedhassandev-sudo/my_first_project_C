aa508a30cfa858350aac5ca003ab127b9fc425fc
mahmoudmohamedhassandev-sudo <mahmoud.mohamed.hassan.dev@gmail.com>
2026-05-11 06:45:00 +0300

    Create my_first_project_python .py

---
+++ my_first_project_python .py
@@ -0,0 +1,16 @@
+Doctors_dec={
+    "Dr_slim":"Tc_English",
+    "Dr_aboud":"Physics",
+    "Dr_Eman":"IT",
+    "Dr_yasmine":"Python",
+    "Dr_nermen":"Maths",
+    "Dr_shrif":"Cyber_security"
+}
+# print(Doctors_dec.items())
+# print(Doctors_dec.values())
+# print(Doctors_dec.keys())
+for j,(doctor, subject), in enumerate(Doctors_dec.items(),
+start=1):
+    print(j,(Doctors_dec,"=>",subject))
+    
+    
